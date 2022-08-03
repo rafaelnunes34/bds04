16d698ceae50d6ad0a90530edfe4bd9081932253
rafaelnunes34 <rafael.nunes34@gmail.com>
2022-08-03 15:48:23 -0300

    Implementação de autorizações de rotas

--- src/main/java/com/devsuperior/bds04/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/bds04/config/ResourceServerConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -17,7 +18,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private Environment env;
 	
-	//private static final String[] URI_PUBLIC = {"/h2-console/**"};
+	private static final String[] URI_PUBLIC = { "/oauth/token","/h2-console/**" };
+	private static final String[] URI_GET_PUBLIC = { "/cities/**","/events/**" };
+	private static final String[] URI_POST_CLIENT = { "/events/**" };
 	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -32,7 +35,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 			http.headers().frameOptions().disable();
 		}
 		
-		http.authorizeRequests().antMatchers("/**").permitAll();
+		http.authorizeRequests()
+			.antMatchers(URI_PUBLIC).permitAll()
+			.antMatchers(HttpMethod.GET, URI_GET_PUBLIC).permitAll()
+			.antMatchers(HttpMethod.POST, URI_POST_CLIENT).hasAnyRole("CLIENT", "ADMIN")
+			.anyRequest().hasAnyRole("ADMIN");
 	}
 	
 	
